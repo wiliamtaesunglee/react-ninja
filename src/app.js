33f01fb7720c14406f192bc9aa4fbcf039d3284f
wil.i.am <wiliam.tae@gmail.com>
2019-07-19 10:22:25 -0300

    class component sintaxe

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 "use strict";
 
-import React from "react";
+import React, { Component } from "react";
 import Title from "./Title";
 
-const App = React.createClass({
-  render: function() {
+// const App = React.createClass({
+//   render: function() {
+//     return (
+//       <div>
+//         <Title />
+//       </div>
+//     );
+//   }
+// });
+
+class App extends Component {
+  render() {
     return (
-      <div>
-        <Title />
+      <div className="container">
+        <Title name="dizzie" />
       </div>
     );
   }
-});
+}
 
 export default App;
