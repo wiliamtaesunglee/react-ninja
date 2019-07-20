fcbce5e54c9cea5627503f4640082e63d5e5696a
wil.i.am <wiliam.tae@gmail.com>
2019-07-20 17:12:41 -0300

    lifecycle

---
+++ src/Timer.js
@@ -0,0 +1,34 @@
+"use strict";
+
+import React, { Component } from "react";
+
+class Timer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      time: 0
+    };
+    this.timer;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log("componenWillReceiveProps", this.props, nextProps);
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(
+      () => this.setState({ time: this.state.time + 1 }),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    return <div>Timer : {this.state.time}</div>;
+  }
+}
+
+export default Timer;
--- src/app.js
+++ src/app.js
@@ -3,24 +3,32 @@
 import React, { Component } from "react";
 import Square from "./Square";
 import Button from "./Button";
+import Timer from "./Timer";
 
 class App extends Component {
   constructor() {
+    console.log("contructor");
     super(); //possibilita o uso do setState() o qual atualiza a rederizacao quando o estado é transformada/alterado
     this.state = {
-      color: "green"
+      time: 0,
+      showTimer: true
     };
   }
+
   render() {
+    console.log("render");
     return (
       <div>
         <Square color={this.state.color} />
-
         {["red", "green", "blue"].map(color => (
           <Button key={color} handleClick={() => this.setState({ color })}>
             {color}
           </Button>
         ))}
+        <Timer time={this.state.time} />
+        <button onClick={() => this.setState({ time: this.state.time + 1 })}>
+          Change Props
+        </button>
       </div>
     );
   }
